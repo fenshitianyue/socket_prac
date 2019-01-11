766eb27f4e29ec57ffe31bb32135b94bec2fea65
fenshitianyue <abzanda@163.com>
2019-01-12 00:15:18 +0800

    update code

---
+++ show_data.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include "sniffer.h"
+#include "tools.h"
+
+
+
