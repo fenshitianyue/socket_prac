d0b7b1f48c6b158acbe3589c720d43263750e528
fenshitianyue <abzanda@163.com>
2019-01-11 23:49:42 +0800

    commit tool file

---
+++ tools.h
@@ -0,0 +1,11 @@
+#pragma once
+//定义了控制颜色和清屏的工具
+
+
+#define CLEARSCREEN() printf("\033[H\033[2J")
+#define INITCOLOR(color) printf("\033[%sm", color);
+#define RED_COLOR "31"
+#define GREEN_COLOR "32"
+#define YELLOW_COLOR "33"
+#define BLUE_COLOR "34"
+#define ZERO_COLOR "0"
